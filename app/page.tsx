dd543d4871ccdb335d24f065d2ee0aca7d43f518
Liaqat Paindah <liaqat.paindah@example.com>
2025-05-20 02:06:04 +0430

    Conflict resolved successfully

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,6 @@ import React from 'react'
 
 const page = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      Main Page Revised 2025
-    </div>
-=======
     <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
     <h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
         Coming Soon
@@ -15,7 +10,6 @@ const page = () => {
         We are working hard to bring you something amazing. Stay tuned!
     </p>
   </div>
->>>>>>> 8ba28b7246ea0640b353eaaf69aa7b62e56cbad4
   )
 }
 
