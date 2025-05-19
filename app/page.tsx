260361c5776f0f47dc2c82920f440bd3ceca4dd0
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2025-05-20 02:22:54 +0430

    about and login added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
@@ -9,6 +10,9 @@ const page = () => {
     <p className="text-white text-lg mb-8">
         We are working hard to bring you something amazing. Stay tuned!
     </p>
+    <Link href="/about">About</Link>
+    <Link href="/auth/login">login</Link>
+
   </div>
   )
 }
