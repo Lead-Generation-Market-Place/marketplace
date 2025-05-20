cfa45749460414dce3698fa7b169a920cd4a1b71
Liaqat Paindah <liaqat.paindah@example.com>
2025-05-20 19:04:09 +0430

    DB configration and DB Schema added

---
+++ libs/utils/auth-helpers.ts
@@ -0,0 +1,47 @@
+import { createClient } from "@/utils/supabase/client";
+
+
+
+export async function verifyOtp()
+{
+
+}
+
+
+export async function login(email: string, password: string)
+{
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    }); 
+    if (error) {
+        throw new Error(error.message);
+    }
+    return data;
+}
+
+
+
+export async function logout()
+{
+    const supabase = createClient();
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+        throw new Error(error.message);
+    }
+}
+
+
+
+
+export async function updatePassword(password: string)
+{
+ const supabase = createClient();
+ const { error } = await supabase.auth.updateUser({
+  password
+ });
+    if (error) {    
+     throw new Error(error.message);
+    }
+}
